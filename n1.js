9669371dd6be6d0849c4bbf5b2c539a877fd31ea
renwenhao <1097481091@qq.com>
2018-06-01 00:47:41 +0800

    second commit

--- n1.js
+++ n1.js
@@ -61,5 +61,78 @@ function exec() {
         }
     })
 }
-exec();
+//并行无关联
+function exec1() {
+    async.parallel({
+        one: function (done) {
+            var ii = 0;
+            setInterval(function () {
+                console.log('aaa=' + new Date());
+                ii++;
+                if (ii == 3) {
+                    clearInterval(this);
+                    done("good game", 'one执行完毕');
+                }
+            }, 1000);
+
+        },
+        two: function (done) {
+            var ii = 0;
+            setInterval(function () {
+                console.log('bbb=' + new Date());
+                ii++;
+                if (ii == 3) {
+                    clearInterval(this);
+                    done(null, 'two执行完毕');
+                }
+            }, 1000);
+
+        }
+    }, function (err, data) {
+        if (!err) {
+            console.log(data);
+        } else {
+            console.log(err);
+        }
+    })
+}
+//串行有关联代码
+function exec2() {
+    async.waterfall([
+        function (done) {
+            var ii = 0;
+            setInterval(function () {
+                console.log('aaa=' + new Date());
+                ii++;
+                if (ii == 3) {
+                    clearInterval(this);
+                    done(null, 'one执行完毕');
+                }
+            }, 1000);
+
+        },
+        function (prev,done) {
+            var ii = 0;
+            setInterval(function () {
+                console.log(prev + '=' + new Date());
+                ii++;
+                if (ii == 3) {
+                    clearInterval(this);
+                    done(null, 'two执行完毕');
+                }
+            }, 1000);
+
+        }
+    ],
+    function (err, data) {
+        if (!err) {
+            console.log(data);
+        } else {
+            console.log(err);
+        }
+    })
+}
+// exec();
+// exec1();
+exec2();
 console.log('主程序执行完毕');
